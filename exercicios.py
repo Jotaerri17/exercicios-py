23fa39ba4b96d28baa274f30cf1023b721a5e4d7
Jotaerri17 <joaorafaelsantos@gmail.com>
2025-02-15 11:49:48 -0300

    Initial commit

---
+++ exercicios.py
@@ -0,0 +1,92 @@
+#n=int(input('digite um valor:'))
+#a=n-1
+#s=n+1
+#d=n*2
+#t=n*3
+#r=n**0.5
+#print(f'O sucessor de {n}, é igual a {s}  e o antecessor é {a}')
+#print(f'O dobro de {n} é {d}, o triplo é igual a {t} e a raiz quadrada é igual a {r}')
+
+
+#d=float(input('Quantos metros de distancia da sua casa para a faculdade?'))
+#c=d*60
+#m=d*1000
+#k=d/1000
+#print(f'A disntancia da sua casa ate a escola em kilometros é de {k}, em centimetros são {c} e em milimetros são {m}')
+
+#r=float(input('quanto vc tem na conta agora?')
+#d=r/6
+#print(f'Com esse valor vc poderia comprar aproximadamente {d}')
+
+#a=float(input('Qual a largura da parede em metros?'))
+#l=float(input('Quanto mede a altura da parede em metros?'))
+#A=a*l
+#t=A/2
+#print(f'Sua area é de {A} metros quadrados')
+#print(f'A quantidade de tinta necessaria sera de {t} litros')
+
+#v=float(input('Qual o alor do produto?'))
+#vr=v-(v*0.05)
+#print(f'O valor do produto com desconto é de {vr}')
+
+#s=float(input('Qual o seu salario?'))
+#ns=s+(s*0.15)
+#print(f'Seu salario apos o reajuste é de {ns}')
+
+#num=int(input('Digite um numero para ver sua tabuada'))
+#print('-'*15)
+#print(f'{num} x 1 = {num*1} \n{num} x 2 = {num*2} \n{num} x 3 = {num*3} \n{num} x 4 = {num*4} \n{num} x 5 = {num*5}')
+#print('-'*15)
+
+#D=int(input('Quantos dias vc alugou o carro?'))
+#K=float(input('Quantos km vc rodou com o carro?'))
+#v= (D*100) + (K*30)
+#print(f'O valor referente ao aluguel ficou de {v}')
+
+import math
+#from math import sqrt
+import random
+#num=int(input('Digite um numero qualquer'))
+#num= random.randint(1,100)
+#raiz=math.sqrt(num)
+#print(f'A raiz do numero {num} é igual {raiz}')
+
+import random
+import math
+from math import floor
+#num=float(input('Digite um numero em casa decimal'))
+#print(f'O numero {num} inteiro é {floor(num)}')
+
+#catop=int(input('Qual o valor do cateto oposto?'))
+#catadj=int(input('Qual o valor do cateto adjacente?'))
+#hip= (catop ** 2) + (catadj ** 2)
+#hip2= math.sqrt(hip)
+#print(f'O valor da Hipotenusa é de {hip2}')
+
+import math
+#an=float(input('Qual o valor do angulo?'))
+#sen= math.sin(math.radians(an))
+#cos=math.cos(math.radians(an))
+#tang=math.tan(math.radians(an))
+#print(f'O seno de {an} é igual a {sen}, o cosseno é {cos} e a tangente é igual a {tang}')
+
+import random
+#n1=input('Primeiro aluno')
+#n2=input('Segundo aluno')
+#n3=input('Terceiro aluno')
+#n4=input('Quarto aluno')
+#Lista= [n1, n2, n3, n4]
+#escolhido=random.choice(Lista)
+#print(f'O sorteado foi {escolhido}')
+
+import random
+from random import shuffle
+
+# n1=input('Primeiro aluno')
+# n2=input('Segundo aluno')
+# n3=input('Terceiro aluno')
+# n4=input('Quarto aluno')
+# Lista= [n1, n2, n3, n4]
+# random,shuffle(Lista)
+# print("A orde de apresentação sera")
+# print(Lista)
