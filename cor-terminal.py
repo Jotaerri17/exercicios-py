b6792d28dede0e56554ed0d66b5e99b2fcd5979f
Jotaerri17 <joaorafaelsantos@gmail.com>
2025-02-17 22:31:57 -0300

    atualizações

---
+++ cor-terminal.py
@@ -0,0 +1,14 @@
+# print('\033[1;30;45mhello world!\033[m')
+
+# a=10
+# b=20
+# print(f'Os valores são \033[0;31m{a} e \033[0;31m{b}')
+
+# print('-='*20)
+# k = int(input('Quantos km/h você estava? '))
+# print('-='*20)
+# e = float(input('E por quanto tempo você se manteve nessa velocidade? '))
+# print('-='*20)
+# print(' \033[0;36mCalculando...\033[m')
+# c = k*e
+# print(f'Pelos meus calculos você percorreu {c}km/h.')
